bc84019ba79d14401d58d057c5e3c50679bd90ab
Yaliang Wu <ylwu@amazon.com>
2025-05-12 12:34:31 -0700

    bind ip address

--- ml-algorithms/src/main/java/org/opensearch/ml/engine/httpclient/MLHttpClientFactory.java
+++ ml-algorithms/src/main/java/org/opensearch/ml/engine/httpclient/MLHttpClientFactory.java
@@ -7,36 +7,141 @@ package org.opensearch.ml.engine.httpclient;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import io.netty.channel.ChannelFactory;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.log4j.Log4j2;
+import software.amazon.awssdk.http.async.AsyncExecuteRequest;
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.http.nio.netty.SdkEventLoopGroup;
 
 @Log4j2
 public class MLHttpClientFactory {
 
-    public static SdkAsyncHttpClient getAsyncHttpClient(Duration connectionTimeout, Duration readTimeout, int maxConnections) {
-        try {
-            return AccessController
-                .doPrivileged(
-                    (PrivilegedExceptionAction<SdkAsyncHttpClient>) () -> NettyNioAsyncHttpClient
-                        .builder()
-                        .connectionTimeout(connectionTimeout)
-                        .readTimeout(readTimeout)
-                        .maxConcurrency(maxConnections)
-                        .build()
+    /**
+     * Custom HTTP client implementation that ensures connections are made from the eth1 interface
+     */
+    private static class Eth1BoundHttpClient implements SdkAsyncHttpClient {
+        private final SdkAsyncHttpClient delegate;
+        private final InetAddress localAddress;
+
+        public Eth1BoundHttpClient(Duration connectionTimeout, Duration readTimeout, int maxConnections, InetAddress localAddress) {
+            this.localAddress = localAddress;
+            try {
+                // Create a custom channel factory that will bind outgoing connections to our eth1 address
+                ChannelFactory<NioSocketChannel> channelFactory = () -> {
+                    NioSocketChannel channel = new NioSocketChannel() {
+                        @Override
+                        protected boolean doConnect(java.net.SocketAddress remoteAddress, java.net.SocketAddress localAddress) throws Exception {
+                            // Force outgoing connections to use our eth1 address
+                            if (localAddress == null) {
+                                localAddress = new java.net.InetSocketAddress(Eth1BoundHttpClient.this.localAddress, 0);
+                            }
+                            return super.doConnect(remoteAddress, localAddress);
+                        }
+                    };
+                    return channel;
+                };
+
+                // Create a custom EventLoopGroup builder with our channel factory
+                SdkEventLoopGroup.Builder eventLoopGroupBuilder = SdkEventLoopGroup.builder()
+                        .channelFactory(channelFactory);
+
+                // Build the delegate HTTP client with our custom event loop group
+                this.delegate = AccessController.doPrivileged(
+                        (PrivilegedExceptionAction<SdkAsyncHttpClient>) () -> NettyNioAsyncHttpClient.builder()
+                                .connectionTimeout(connectionTimeout)
+                                .readTimeout(readTimeout)
+                                .maxConcurrency(maxConnections)
+                                .eventLoopGroupBuilder(eventLoopGroupBuilder)
+                                .putChannelOption(ChannelOption.SO_REUSEADDR, true)
+                                .build()
                 );
+            } catch (Exception e) {
+                log.error("Failed to create eth1-bound HTTP client", e);
+                throw new RuntimeException("Failed to initialize eth1-bound HTTP client", e);
+            }
+        }
+
+        @Override
+        public CompletableFuture<Void> execute(AsyncExecuteRequest request) {
+            return delegate.execute(request);
+        }
+
+        @Override
+        public void close() {
+            delegate.close();
+        }
+
+        @Override
+        public String clientName() {
+            return "Eth1Bound-" + delegate.clientName();
+        }
+    }
+
+    private static SdkAsyncHttpClient createDefaultHttpClient(Duration connectionTimeout, Duration readTimeout, int maxConnections) {
+        try {
+            return AccessController.doPrivileged(
+                    (PrivilegedExceptionAction<SdkAsyncHttpClient>) () -> NettyNioAsyncHttpClient.builder()
+                            .connectionTimeout(connectionTimeout)
+                            .readTimeout(readTimeout)
+                            .maxConcurrency(maxConnections)
+                            .build()
+            );
         } catch (PrivilegedActionException e) {
+            log.error("Failed to create HTTP client", e);
             return null;
         }
+    }
+
+    public static SdkAsyncHttpClient getAsyncHttpClient(Duration connectionTimeout, Duration readTimeout, int maxConnections) {
+        try {
+            // Try to get the IPv4 address of eth1
+            InetAddress localAddress = getEth1Address();
+            if (localAddress != null) {
+                log.info("Creating HTTP client bound to eth1 interface with address: {}", localAddress.getHostAddress());
+                return new Eth1BoundHttpClient(connectionTimeout, readTimeout, maxConnections, localAddress);
+            } else {
+                log.warn("Could not find eth1 interface or IPv4 address. Using default interface for HTTP client.");
+                return createDefaultHttpClient(connectionTimeout, readTimeout, maxConnections);
+            }
+        } catch (Exception e) {
+            log.error("Failed to create HTTP client with eth1 binding, falling back to default", e);
+            return createDefaultHttpClient(connectionTimeout, readTimeout, maxConnections);
+        }
+    }
+
+    private static InetAddress getEth1Address() {
+        try {
+            NetworkInterface eth1 = NetworkInterface.getByName("eth1");
+            if (eth1 != null) {
+                Enumeration<InetAddress> addresses = eth1.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (address instanceof Inet4Address) {
+                        return address;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("Error finding eth1 interface", e);
+        }
+        return null;
     }
 
     /**
