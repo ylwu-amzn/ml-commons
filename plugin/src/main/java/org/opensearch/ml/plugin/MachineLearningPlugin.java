5a39a91d2f4cc397da8edaf8aa7bf01fdc420672
Yaliang Wu <ylwu@amazon.com>
2025-10-29 14:27:59 -0700

    create internal connector in memory container
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
+++ plugin/src/main/java/org/opensearch/ml/plugin/MachineLearningPlugin.java
@@ -285,7 +285,9 @@ import org.opensearch.ml.engine.tools.VisualizationsTool;
 import org.opensearch.ml.engine.tools.WriteToScratchPadTool;
 import org.opensearch.ml.engine.utils.AgentModelsSearcher;
 import org.opensearch.ml.helper.ConnectorAccessControlHelper;
+import org.opensearch.ml.helper.MemoryContainerPipelineHelper;
 import org.opensearch.ml.helper.ModelAccessControlHelper;
+import org.opensearch.ml.helper.RemoteMemoryStoreHelper;
 import org.opensearch.ml.jobs.MLJobParameter;
 import org.opensearch.ml.jobs.MLJobRunner;
 import org.opensearch.ml.memory.ConversationalMemoryHandler;
@@ -523,6 +525,8 @@ public class MachineLearningPlugin extends Plugin
     private Encryptor encryptor;
     private McpToolsHelper mcpToolsHelper;
     private McpStatelessServerHolder statelessServerHolder;
+    private RemoteMemoryStoreHelper remoteMemoryStoreHelper;
+    private MemoryContainerPipelineHelper memoryContainerPipelineHelper;
 
     public MachineLearningPlugin() {}
 
@@ -914,6 +918,16 @@ public class MachineLearningPlugin extends Plugin
         mcpToolsHelper = new McpToolsHelper(client, toolFactoryWrapper);
         statelessServerHolder = new McpStatelessServerHolder(mcpToolsHelper, client, threadPool);
 
+        remoteMemoryStoreHelper = new RemoteMemoryStoreHelper(
+            client,
+            clusterService,
+            scriptService,
+            xContentRegistry,
+            encryptor,
+            mlFeatureEnabledSetting,
+            mlIndicesHandler
+        );
+        memoryContainerPipelineHelper = new MemoryContainerPipelineHelper(client, mlIndicesHandler, remoteMemoryStoreHelper);
         return ImmutableList
             .of(
                 encryptor,
@@ -945,7 +959,9 @@ public class MachineLearningPlugin extends Plugin
                 sdkClient,
                 toolFactoryWrapper,
                 mcpToolsHelper,
-                statelessServerHolder
+                statelessServerHolder,
+                remoteMemoryStoreHelper,
+                memoryContainerPipelineHelper
             );
     }
 
